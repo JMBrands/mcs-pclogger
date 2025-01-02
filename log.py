d9dc7266bb4f2975589c45986114b634eae2000d
JMBrands <jonar@ultramagic.net>
2025-01-02 14:23:24 +0100

    hi

--- log.py
+++ log.py
@@ -2,18 +2,23 @@
 import json
 import checkplayers
 import sched, time
+import MySQLdb as mysql
+import os
+import dotenv
+import sys
+
+dotenv.load_dotenv()
 
 def log(scheduler: sched.scheduler):
-    scheduler.enter(3600, 1, log, (scheduler,))
-    data: dict
-    with open("data.json", 'r') as f:
-        data = json.load(f)
-    data["data"].append({"time": time.time() * 1000, "players": checkplayers.checkplayers("noob-friendly.com")})
+    scheduler.enter(900 - (time.time() % 900), 1, log, (scheduler,))
+    sql = mysql.connect("localhost", "nf-monitor", os.getenv("SQLPASS"), "nf-monitor")
+    # sql.query(f"INSERT INTO PlayerCount (Time, PlayerCount) VALUE (NOW(), 0);")
+    count = checkplayers.checkplayers('noob-friendly.com')
+    print(count)
+    sql.query(f"INSERT INTO `PlayerCount` (`Time`, `PlayerCount`) VALUE (NOW(), {count});")
+    sql.commit()
     
-    with open("data.json", 'w') as f:
-        json.dump(data, f)
-
 if __name__ == "__main__":
     logger = sched.scheduler(time.time, time.sleep)
-    logger.enter(time.time() % 3600, 1, log, (logger,))
+    logger.enter(0 if "debug" in sys.argv else 900 - (time.time() % 900), 1, log, (logger,))
     logger.run()
